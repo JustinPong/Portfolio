5072b2462169cbfddf2a71d7b35dce4813eb57f4
JustinPong <Justinpong0416@gmail.com>
2022-02-21 05:08:41 +0800

    fixed homepage content position

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './Nav.css';
 import { Link } from 'react-router-dom';
-import Pdf from './JustinPong CV with VASK.pdf';
+import Pdf from "./JustinPong CV with VASK.pdf";
 
 function Nav() {
     return (
