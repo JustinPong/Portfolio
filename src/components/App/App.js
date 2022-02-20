653da0115e225310936a40f21f54c39ce690ac69
JustinPong <Justinpong0416@gmail.com>
2022-02-20 13:10:16 +0800

    added basename

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL} >
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
